52c65599cb3db47b38f1ee235bad392b5eac2878
tjcook <tjcook2327@gmail.com>
2025-12-15 22:57:28 -0600

    Project Finished

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 from stats import get_book_text, get_num_words, char_num_times, sort_list
+import sys
 
 def main():
-    book = get_book_text("books/frankenstein.txt")
-    
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count ----------")
+    if len(sys.argv) < 2:
+        print(f"Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book = get_book_text(sys.argv[1])
+        
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {sys.argv[1]}...")
+        print("----------- Word Count ----------")
 
-    print(f"Found {get_num_words(book)} total words")
-    num_chars = char_num_times(book)
-    print("--------- Character Count -------")
+        print(f"Found {get_num_words(book)} total words")
+        num_chars = char_num_times(book)
+        print("--------- Character Count -------")
 
-    sorted_list = sort_list(num_chars)
-    for key, value in sorted_list.items():
-        print(f"{key}: {value}")
-    print("============= END ===============")
-    
+        sorted_list = sort_list(num_chars)
+        for key, value in sorted_list.items():
+            print(f"{key}: {value}")
+        print("============= END ===============")
+
+
 main()
--- stats.py
+++ stats.py
@@ -1,11 +1,13 @@
+#Open a file and return the contents
 def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
     return file_contents
+#Use the contents of a book file and return the number of words in the file
 def get_num_words(book_obj):
     words = len(book_obj.split())
     return words
-    
+#Iterate through list and return a dictionary of characters and  number times a character is in the file
 def char_num_times(book_obj):
     char_list = {}
     for char in book_obj:
@@ -17,10 +19,9 @@ def char_num_times(book_obj):
         else:
             char_list[char] = 1
     return char_list
-
+#Sort a character dictionary by the number of times character is in dict most to least
 def sort_on(items):
     return items
-
 def sort_list(char_dict):
     char_list = []
     char_sorted_dict = {}
